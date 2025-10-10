b711242cccc5dffcceffb2495f0957e76be5397b
Ga-Zero <rrtu123@naver.com>
2025-10-10 22:28:59 +0900

    [✨][Feat]#36: 비밀번호 찾기 구현

---
+++ src/features/auth/AuthForm.tsx
@@ -0,0 +1,209 @@
+import Input from '@/components/Input'
+import Button from '@/components/Button'
+import { useTimer } from '@/hooks/useTimer'
+import { useState } from 'react'
+import { formatTime } from '@/utils/time'
+import type { useAuthForm } from '@/hooks/useAuthForm'
+
+type AuthFormProps = ReturnType<typeof useAuthForm> & {
+  submitButtonLabel: string
+  mode: 'register' | 'passwordReset' // 회원가입 또는 비밀번호 찾기 모드
+}
+const AuthForm = ({
+  email,
+  setEmail,
+  password,
+  confirmPassword,
+  passwordMatchError,
+  isLoading,
+  emailVerifyMessage,
+  verificationCode,
+  setVerificationCode,
+  isEmailVerified,
+  handlePasswordChange,
+  handleConfirmPasswordChange,
+  handleSendVerification,
+  handleConfirmVerification,
+  handleSubmit,
+  emailConfirmError,
+  submitButtonLabel,
+  mode,
+}: AuthFormProps) => {
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  // 타이머 훅
+  const verificationTimer = useTimer(300)
+  const resendTimer = useTimer(30, false)
+
+  const handleSendVerificationWithTimer = async () => {
+    // API 호출 전, 기존 메시지 초기화
+    setEmailError('')
+
+    // handleSendVerification을 호출하고 성공 여부를 받습니다.
+    const isSuccess = await handleSendVerification()
+
+    if (isSuccess) {
+      verificationTimer.start()
+      resendTimer.start()
+    }
+  }
+
+  if (isEmailVerified && verificationTimer.isActive) {
+    verificationTimer.stop()
+  }
+
+  // 비밀번호 찾기에서 버튼 활성화 조건에 isEmailVerified를 포함
+  const isPasswordResetFieldsFilled =
+    isEmailVerified && // 이메일 인증 완료 필수
+    password && // 비밀번호 입력 필수
+    confirmPassword && // 비밀번호 재확인 입력 필수
+    !passwordMatchError &&
+    !passwordError
+
+  const isRegisterFieldsFilled =
+    email &&
+    password &&
+    confirmPassword &&
+    verificationCode &&
+    !emailError &&
+    !passwordError &&
+    !passwordMatchError &&
+    isEmailVerified
+
+  const isButtonDisabled =
+    isLoading ||
+    (mode === 'register'
+      ? !isRegisterFieldsFilled
+      : !isPasswordResetFieldsFilled)
+
+  // 인증이 완료되었을 때
+  const showPasswordFields =
+    mode === 'register' || (mode === 'passwordReset' && isEmailVerified)
+
+  // 인증이 완료되지 않았을 때
+  const showAuthFields =
+    mode === 'register' || (mode === 'passwordReset' && !isEmailVerified)
+
+  // 비밀번호 찾기 모드에서 인증 완료 시 이메일/인증 필드는 비활성화
+  const isAuthFieldsDisabled = isEmailVerified && mode === 'passwordReset'
+
+  return (
+    <form onSubmit={handleSubmit} className='flex flex-col'>
+      <div className='space-y-7'>
+        {showAuthFields && (
+          <>
+            {/* 이메일 입력 */}
+            <div className='flex items-end space-x-2 w-full'>
+              <Input
+                label='이메일'
+                name='email'
+                type='email'
+                placeholder='이메일'
+                required
+                onChange={(value) => setEmail(value)}
+                containerClassName='flex-1'
+                errorMessage={emailError}
+                showError={!!emailError}
+                onErrorChange={setEmailError}
+                disabled={isAuthFieldsDisabled}
+              />
+              <Button
+                label={
+                  resendTimer.isActive
+                    ? `${resendTimer.timeLeft}초 후 재전송`
+                    : '인증번호 전송'
+                }
+                type='button'
+                size='m'
+                baseButton
+                onClick={handleSendVerificationWithTimer}
+                disabled={
+                  !!emailError ||
+                  !email ||
+                  isAuthFieldsDisabled ||
+                  resendTimer.isActive
+                }
+              />
+            </div>
+
+            {/* 이메일 인증번호 입력 */}
+            <div className='flex items-end space-x-2 w-full'>
+              <Input
+                label='이메일 인증 번호'
+                name='emailConfirm'
+                type='text'
+                placeholder='이메일 인증 번호'
+                onChange={(v) => setVerificationCode(v)}
+                containerClassName='flex-1'
+                errorMessage={
+                  emailConfirmError ? (
+                    <span className='text-red_one'>{emailConfirmError}</span>
+                  ) : undefined
+                }
+                showError={!!emailVerifyMessage || !!emailConfirmError}
+                timer={
+                  verificationTimer.isActive
+                    ? formatTime(verificationTimer.timeLeft)
+                    : null
+                }
+                disabled={isAuthFieldsDisabled}
+              />
+              <Button
+                label='인증 확인'
+                type='button'
+                size='m'
+                baseButton
+                onClick={handleConfirmVerification}
+                disabled={!verificationCode || isAuthFieldsDisabled}
+              />
+            </div>
+          </>
+        )}
+        {showPasswordFields && (
+          <>
+            {/* 비밀번호 입력 */}
+            <Input
+              label='비밀번호'
+              name='password'
+              type='password'
+              placeholder='비밀번호'
+              required
+              onChange={handlePasswordChange}
+              errorMessage={passwordError}
+              showError={!!passwordError}
+              onErrorChange={setPasswordError}
+            />
+
+            {/* 비밀번호 재확인 */}
+            <Input
+              label='비밀번호 재확인'
+              name='confirmPassword'
+              type='password'
+              placeholder='비밀번호 재확인'
+              required
+              isConfirmPassword
+              onChange={handleConfirmPasswordChange}
+              errorMessage={passwordMatchError}
+              showError={!!passwordMatchError}
+            />
+          </>
+        )}
+      </div>
+      {/* 최종 제출 버튼 */}
+      {showPasswordFields && (
+        <div className='pt-10'>
+          <Button
+            type='submit'
+            baseButton
+            label={submitButtonLabel}
+            loading={isLoading}
+            disabled={isButtonDisabled}
+          />
+        </div>
+      )}
+    </form>
+  )
+}
+
+export default AuthForm
