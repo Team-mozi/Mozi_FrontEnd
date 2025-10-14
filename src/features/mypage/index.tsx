2ba42e5c20bdc05d51f1d848bb167b2d1a1e465c
Ga-Zero <rrtu123@naver.com>
2025-10-14 14:01:32 +0900

    [✨][Feat]#53: 마이페이지 반응형 구현

--- src/features/mypage/index.tsx
+++ src/features/mypage/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import SideSheet from '@/components/SideSheet'
+import BottomSheet from '@/components/BottomSheet'
 import Button from '@/components/Button'
 import { useSelector } from 'react-redux'
 import type { RootState } from '@/store/store'
 import LogoutModal from '@/features/modal/LogoutModal'
 import WithdrawalModal from '@/features/modal/WithdrawalModal'
+import useMobile from '@/hooks/useMobile'
+import Logo from '@/components/Logo'
 
 const MyPage = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -12,29 +15,44 @@ const MyPage = () => {
   const [isWithdrawalModalOpen, setIsWithdrawalModalOpen] = useState(false)
   const { nickname, email } = useSelector((state: RootState) => state.auth)
 
-  return (
-    <div>
-      {/* 추후 홈에서 마이페이지 버튼 추가하면 삭제 예정 */}
-      <Button
-        label='마이페이지'
-        onClick={() => setIsOpen(true)}
-        baseButton
-        size='s'
-      />
+  const isMobile = useMobile() // 현재 화면이 모바일인지 확인
 
-      <SideSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        {/* 마이페이지 컨텐츠 영역 */}
+  // 공통 콘텐츠 (SideSheet / BottomSheet 둘 다에서 사용)
+  const sheetContent = (
+    <>
+      {isMobile ? (
+        <BottomSheet.Content>
+          <Logo size='s'></Logo>
+          <p className='text-lg pb-1'>{nickname} 님</p>
+          <p className='text-lg'>{email}</p>
+        </BottomSheet.Content>
+      ) : (
         <SideSheet.Content>
-          <div className='p-12 space-y-4'>
-            <p className='text-5xl font-extrabold text-orange_five'>MOZI</p>
-            <p className='text-xl'>{nickname} 님</p>
+          <div className='px-10'>
+            <Logo size='l'></Logo>
+            <p className='text-xl pb-2'>{nickname} 님</p>
             <p className='text-xl'>{email}</p>
           </div>
         </SideSheet.Content>
-
-        {/* 마이페이지 푸터 버튼 영역 */}
+      )}
+      {/* 푸터 버튼 */}
+      {isMobile ? (
+        <BottomSheet.Footer>
+          <div className='space-y-4'>
+            <Button
+              label='로그아웃'
+              baseButton
+              onClick={() => setIsLogoutModalOpen(true)}
+            />
+            <Button
+              label='회원탈퇴'
+              onClick={() => setIsWithdrawalModalOpen(true)}
+            />
+          </div>
+        </BottomSheet.Footer>
+      ) : (
         <SideSheet.Footer>
-          <div className='p-12 space-y-6'>
+          <div className='p-10 space-y-6'>
             <Button
               label='로그아웃'
               baseButton
@@ -46,7 +64,30 @@ const MyPage = () => {
             />
           </div>
         </SideSheet.Footer>
-      </SideSheet>
+      )}
+    </>
+  )
+
+  return (
+    <div>
+      {/* 추후 홈에서 마이페이지 버튼 추가하면 삭제 예정 */}
+      <Button
+        label='마이페이지'
+        onClick={() => setIsOpen(true)}
+        baseButton
+        size='s'
+      />
+
+      {/* 반응형: 화면 크기에 따라 SideSheet / BottomSheet 전환 */}
+      {isMobile ? (
+        <BottomSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          {sheetContent}
+        </BottomSheet>
+      ) : (
+        <SideSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          {sheetContent}
+        </SideSheet>
+      )}
 
       {/* 로그아웃 모달 */}
       <LogoutModal
