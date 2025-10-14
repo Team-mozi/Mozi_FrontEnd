23bdf04e1f0a35a2590526e5e6c4fc7a60919abb
Ga-Zero <rrtu123@naver.com>
2025-10-14 20:00:44 +0900

    [✨][Feat]#42: 마이페이지 버튼 svg 추가

--- src/features/mypage/index.tsx
+++ src/features/mypage/index.tsx
@@ -70,13 +70,17 @@ const MyPage = () => {
 
   return (
     <div>
-      {/* 추후 홈에서 마이페이지 버튼 추가하면 삭제 예정 */}
-      <Button
-        label='마이페이지'
-        onClick={() => setIsOpen(true)}
-        baseButton
-        size='s'
-      />
+      {/* 마이페이지 버튼 */}
+      <button onClick={() => setIsOpen(true)} className='text-black'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 448 512'
+          className='w-8 h-8' // 버튼 사이즈
+          fill='currentColor'
+        >
+          <path d='M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z' />
+        </svg>
+      </button>
 
       {/* 반응형: 화면 크기에 따라 SideSheet / BottomSheet 전환 */}
       {isMobile ? (
